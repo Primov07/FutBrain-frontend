13b0cef496805b992bd4fda6a4608eadb5784443
Primov07 <primov0701@gmail.com>
2026-05-31 15:33:16 +0300

    basename in main

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from './auth/AuthContext.tsx'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename="/FutBrain-frontend">
 			<AuthProvider>
 				<App />
 			</AuthProvider>
